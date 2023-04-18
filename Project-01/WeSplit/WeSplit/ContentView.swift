15d2a7beaeba14dd9f8ebb77445b95409a8cb441
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-18 20:52:57 +0300

    1st challenge

--- Project-01/WeSplit/WeSplit/ContentView.swift
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -65,6 +65,8 @@ struct ContentView: View {
                 
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Amount we each pay")
                 }
             }
             .navigationTitle("WeSplit")
