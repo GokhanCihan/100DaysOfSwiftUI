ff2adf7fe0bf427b3643ccfe8eaee7121cff6c9e
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-18 21:57:00 +0300

    tough challenge: FloatingPointFormatStyle

--- Project-01/WeSplit/WeSplit/ContentView.swift
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -8,39 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+
     @FocusState private var amountIsFocused: Bool
-    
+
+    let currencyLocaleFormat = FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currency?.identifier ?? "USD")
+
     let tipPercentages = [0, 10, 15, 20, 25]
-    
+
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelected = Double(tipPercentage)
         let tipValue = checkAmount * (tipSelected / 100)
         let total = checkAmount + tipValue
         let totalPerPerson = total / peopleCount
-        
+
         return totalPerPerson
     }
-    
+
     var total: Double {
         let tipSelected = Double(tipPercentage)
         let tipValue = checkAmount * (tipSelected / 100)
         let total = checkAmount + tipValue
-        
+
         return total
     }
-    
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Amount", value: $checkAmount, format:
-                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            currencyLocaleFormat)
                     .keyboardType(.decimalPad)
                     .focused($amountIsFocused)
                     
@@ -52,14 +54,14 @@ struct ContentView: View {
                     .toolbar {
                         ToolbarItemGroup(placement: .keyboard) {
                             Spacer()
-                            
+
                             Button("Done") {
                                 amountIsFocused = false
                             }
                         }
                     }
                 }
-                
+
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<101) {
@@ -70,15 +72,15 @@ struct ContentView: View {
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
-                
+
                 Section {
-                    Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(total, format: currencyLocaleFormat)
                 } header: {
                     Text("Amount we split")
                 }
-                
+
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: currencyLocaleFormat)
                 } header: {
                     Text("Amount we each pay")
                 }
