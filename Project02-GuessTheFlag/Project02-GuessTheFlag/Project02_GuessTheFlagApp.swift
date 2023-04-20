d487966ed223372b4f2c3fe401de99cb74a8c428
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-20 21:13:22 +0300

    create Project02

---
+++ Project02-GuessTheFlag/Project02-GuessTheFlag/Project02_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project02_GuessTheFlagApp.swift
+//  Project02-GuessTheFlag
+//
+//  Created by Gökhan on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project02_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
