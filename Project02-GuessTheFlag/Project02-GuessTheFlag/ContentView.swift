45b96e59376108d8768728893e858cfa974e4580
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-21 17:44:39 +0300

    show alert when flags are tapped

--- Project02-GuessTheFlag/Project02-GuessTheFlag/ContentView.swift
+++ Project02-GuessTheFlag/Project02-GuessTheFlag/ContentView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = "score"
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-            Text("Hello World!")
+        ZStack {
+            Color(.init(red: 0.3, green: 0.4, blue: 1, alpha: 0.9))
+                .ignoresSafeArea()
+            
+            VStack {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                    flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: newQuestion)
+        } message: {
+            Text("Your score is ????????")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func newQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
