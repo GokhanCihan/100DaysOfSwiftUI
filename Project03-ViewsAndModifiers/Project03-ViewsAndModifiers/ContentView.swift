0687782942fd232354e7ae78161b10f3d9c86e99
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-23 18:59:17 +0300

    custom view modifiers

--- Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
+++ Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
@@ -76,8 +76,58 @@ struct CompositeView: View {
     }
 }
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct CustomModifierView: View {
+    var body: some View {
+        VStack(spacing: 100) {
+            Text("Text with custom modifier")
+                .modifier(Title())
+            Text("Text with custom modifier extension")
+                .titleStyle()
+        }
+        .watermarked(with: "Watermark Text")
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        CompositeView()
+        CustomModifierView()
     }
 }
