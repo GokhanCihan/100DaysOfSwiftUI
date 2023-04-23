7f769a7c9b378637504667927c6b16389a5df5ed
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-23 18:30:26 +0300

    use composite views

--- Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
+++ Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
@@ -54,8 +54,30 @@ struct PropertiesView: View {
     }
 }
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct CompositeView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        PropertiesView()
+        CompositeView()
     }
 }
