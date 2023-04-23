4bbd38555a47b83898c9e5764ee2b851ace2f0bd
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-23 16:30:42 +0300

    create Project03

---
+++ Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/Project03_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project03_ViewsAndModifiersApp.swift
+//  Project03-ViewsAndModifiers
+//
+//  Created by Gökhan on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project03_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
