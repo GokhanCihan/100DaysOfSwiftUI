cfda2d974619833a58f5dbfe839dfb15c9c9e138
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-23 17:08:22 +0300

    The order modifiers are applied matters

--- Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
+++ Project03-ViewsAndModifiers/Project03-ViewsAndModifiers/ContentView.swift
@@ -9,18 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text("Hello World!")
+    }
+}
+
+struct ExampleView: View {
+    var body: some View {
+        Button("Hello, world!"){
+            print(type(of: self.body))
         }
-        .padding()
+            .frame(maxWidth: 300, maxHeight: 150)
+            .padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ExampleView()
     }
 }
