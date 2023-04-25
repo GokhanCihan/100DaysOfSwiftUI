d1c54b65d7491a9461b9d8bf40083386f1da3804
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-25 19:30:04 +0300

    begin 1st milestone project

---
+++ MilestoneProjects/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsWinOrLoseApp.swift
+//  RockPaperScissorsWinOrLose
+//
+//  Created by Gökhan on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsWinOrLoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
