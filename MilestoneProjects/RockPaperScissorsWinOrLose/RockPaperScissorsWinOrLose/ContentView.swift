5a917ff5ec147ee7cad32a084a816bc1bebdaf9d
Gökhan Cihan <cihangokhan@gmail.com>
2023-04-25 21:32:02 +0300

    compare rock, paper and scissor

--- MilestoneProjects/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLose/ContentView.swift
+++ MilestoneProjects/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLose/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var AIweapon: Weapon = Weapon.allCases.randomElement()!
+
+    @State private var humanWeapon: Weapon = Weapon.allCases.randomElement()!
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("AI: \(AIweapon.rawValue)")
+            Text("Human: \(humanWeapon.rawValue)")
+            Text("Human Wins: \(match(humanWeapon, with: AIweapon).description)")
         }
         .padding()
+    }
+
+    enum Weapon: String, CaseIterable {
+        case rock
+        case paper
+        case scissor
+    }
+
+    func match(_ humanWeapon: Weapon, with AIWeapon: Weapon) -> Bool {
+        if humanWeapon == .rock && AIWeapon == .scissor {
+            return true
+        }else if humanWeapon == .paper && AIWeapon == .rock {
+            return true
+        }else if humanWeapon == .scissor && AIWeapon == .paper {
+            return true
+        }else {
+            return false
+        }
     }
 }
 
