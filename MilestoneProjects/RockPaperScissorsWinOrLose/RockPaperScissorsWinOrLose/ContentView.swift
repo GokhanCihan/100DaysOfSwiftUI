c28387c0860a0ceaeefae64f92c5a6f775f0493f
Gökhan Cihan <cihangokhan@gmail.com>
2023-05-09 13:01:51 +0300

    change property names etc.

--- MilestoneProjects/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLose/ContentView.swift
+++ MilestoneProjects/RockPaperScissorsWinOrLose/RockPaperScissorsWinOrLose/ContentView.swift
@@ -7,61 +7,122 @@
 
 import SwiftUI
 
-struct ContentView: View {
+var handGestures = ["✊", "✋", "✌️"]
 
-    @State private var weapons = ["rock", "paper", "scissors"]
+struct ContentView: View {
     
-    @State private var AIWeapon: String = "paper"
-    @State var humanWeapon: String = "Thinking"
+    let winCondition = Bool.random()
     
-    @State private var AIWeaponSelected = true
-    @State private var humanWeaponSelected = false
+
+    @State private var AIGesture: String = handGestures.randomElement()!
+    @State private var humanGesture: String = "Thinking"
     
-    var matchResult: String {
-        if humanWeapon == "rock" && AIWeapon == "scissors" {
-            return "win"
-        }else if humanWeapon == "paper" && AIWeapon == "rock" {
-            return "win"
-        }else if humanWeapon == "scissors" && AIWeapon == "paper" {
-            return "win"
-        }else if humanWeapon == AIWeapon {
-            return "nothing happens"
+    @State private var AIGestureMade = false
+    @State private var humanGestureMade = false
+
+    @State private var AIScore = 0
+    @State private var humanScore = 0
+
+    @State private var round = 1
+
+    var isHumanGestureCorrect: Bool {
+        if humanGesture == "✊" && AIGesture == "✌️" {
+            return true
+        }else if humanGesture == "✋" && AIGesture == "✊" {
+            return true
+        }else if humanGesture == "✌️" && AIGesture == "✋" {
+            return true
         }else {
-            return "lost"
+            return false
+        }
+    }
+    
+    var isMatchResultCorrect: Bool {
+        if winCondition == isHumanGestureCorrect {
+            return true
+        } else{
+            return false
         }
     }
 
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
-            VStack() {
-                Text("AI: \(AIWeapon)")
-            }
-            .frame(minWidth: 800, minHeight: 500)
-            .border(.black)
-            
-            VStack(alignment: .center, spacing: 50){
-                Text("\(matchResult)")
-                
-                VStack(){
-                    Text("Choose your weapon")
-                    HStack(spacing: 50) {
-                        ForEach(weapons, id: \.self) { weapon in
-                            Button {
-                                selectHumanWeapon(weapon)
-                            } label: {
-                                Text(weapon)
+
+                Section() {
+                    VStack() {
+                        Text(AIGesture)
+                            .font(.system(size: 200))
+                    }
+                    .frame(minWidth: 800, minHeight: 520)
+                    .border(.black)
+                    
+                    VStack() {
+                        HStack() {
+                            Text("AI SCORE: \(AIScore)")
+                            Spacer()
+                            ZStack(alignment: .center){
+                                Text(winCondition ? "Win" : "Lose")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.heavy)
+                                    .padding(
+                                        EdgeInsets(
+                                            top: 10,
+                                            leading: 20,
+                                            bottom: 10,
+                                            trailing: 20
+                                        )
+                                    )
                             }
+                            .frame(alignment: .center)
+                            .background(winCondition ? .green : .red)
+                            
+                            Spacer()
+                            Text("HUMAN SCOR: \(humanScore)")
                         }
                     }
+                    .frame(maxWidth: 800)
+                    
+                    VStack() {
+                        Text("\(isHumanGestureCorrect.description)")
+                        VStack() {
+                            Text("make your move")
+                            HStack(spacing: 45) {
+                                ForEach(handGestures, id: \.self) { handGesture in
+                                    Button() {
+                                        madeHumanGesture(handGesture)
+                                    } label: {
+                                        Text(handGesture)
+                                            .font(.system(size: 200))
+                                            .opacity(changeOpacity(for: handGesture))
+                                    }
+                                }
+                            }
+                            .frame(maxWidth: 800, maxHeight: 300)
+                        }
+                    }
+                    .frame(maxWidth: 800, maxHeight: 520)
+                    .border(.black)
                 }
             }
-            .frame(minWidth: 800, minHeight: 500)
-            .border(.black)
-        }
+            .ignoresSafeArea()
     }
-    func selectHumanWeapon(_ weapon: String) {
-        humanWeaponSelected = true
-        humanWeapon = weapon
+
+    func madeHumanGesture(_ handGesture: String) {
+        humanGestureMade = true
+        humanGesture = handGesture
+        print(humanGesture)
+    }
+    
+    func changeOpacity(for handGesture: String) -> Double {
+        if humanGestureMade {
+            if handGesture == self.humanGesture {
+                return 100
+            }else {
+                return 0
+            }
+        }else {
+            return 100
+        }
     }
 }
 
